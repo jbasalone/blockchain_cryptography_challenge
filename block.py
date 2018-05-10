d5d674ec053b14c55c2a24b5156868b8d781e2f1
Jennifer Basalone <jbasalone@gmail.com>
2018-05-09 20:35:23 -0400

    wrong update from env

--- block.py
+++ block.py
@@ -1,44 +1,52 @@
-OLUTION
+import time
+import os
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.serialization import PublicFormat
+from cryptography.hazmat.primitives.serialization import Encoding
+from cryptography.hazmat.primitives import serialization
 
-# SOLUTION
-
-
-class Blockchain():
-    def __init__(self):
-        self.blocks = []
+class Block():
+    def __init__(self, data, previous_hash, nonce=None):
+        self.data = data
+        
+        #block header
+        self.timestamp = str(int(time.time()))
+        self.previous_hash = previous_hash
+        if nonce:
+            self.nonce= nonce
+        else:
+            self.nonce = base64.b16encode(os.urandom(16))
+        
+        #hash of hte block header + data
+        digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+        digest.update(self.previous_hash)
+        digest.update(str.encode(self.timestamp))
+        digest.update(self.data)
+        digest.update(self.nonce)
+        self.hash = base64.b16encode(digest.finalize())
     
     def __repr__(self):
-        _blocks = []
-        for _block in self.blocks:
-            _blocks.append(str(_block))
-        return "\n\n".join(_blocks)
+        return "PreviousHash: {0}\nTimestamp: {1}\nNonce: {2}\nHash: {3}".format(
+            self.previous_hash, self.timestamp, self.nonce, self.hash)
+
+def hash_pub_key(private_key):
+        digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+        digest.update(private_key.public_key().public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo))
+        return base64.b16encode(digest.finalize())
+
+def sign_tx(tx, private_key):
+    return private_key.sign(tx, ec.ECDSA(hashes.SHA256()))
+
+def serialize_pubkey(publickey):
+    serialized_public = publickey.public_bytes(
+    encoding=Encoding.PEM,
+    format=PublicFormat.SubjectPublicKeyInfo)
     
-    def add_block(self, block):
-        self.blocks.append(block)
-    
-    def blockchain_valid(self):   
-        # check if the genesis block is correct
-        if self.blocks[0] != genesis_block:
-            return False
-        
-        prv_block = self.blocks[0]
-        
-        for block in self.blocks[1:]:
-            # check if the prv_hash of the block points to the prv block
-            if prv_block.hash != block.prv_hash:
-                return False
-            
-            # Check Hash(current+prv) = current hash
-            digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
-            digest.update(block.prv_hash)
-            digest.update(str.encode(block.timestamp))
-            digest.update(block.nonce)
-            digest.update(block.data)
-            hash_digest = base64.b16encode(digest.finalize())
-            
-            if hash_digest != block.hash:
-                return False
-            
-            prv_block = block
-        
-        return True
+    return serialized_public
+
+def parse_serialized_pubkey(serialized_pubkey):
+    loaded_public_key = serialization.load_pem_public_key(serialized_pubkey, backend=default_backend())
+    return loaded_public_key
